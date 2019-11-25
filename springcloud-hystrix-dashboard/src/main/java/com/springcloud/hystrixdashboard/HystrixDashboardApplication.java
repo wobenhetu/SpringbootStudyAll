4a1cc7367e4201af9c8632d4f4f48161f9b24be2
dengfenwu <dengfenwu@ysstech.com>
2019-11-25 13:24:07 +0800

    dd

--- springcloud-hystrix-dashboard/src/main/java/com/springcloud/hystrixdashboard/HystrixDashboardApplication.java
+++ springcloud-hystrix-dashboard/src/main/java/com/springcloud/hystrixdashboard/HystrixDashboardApplication.java
@@ -1,11 +1,8 @@
 package com.springcloud.hystrixdashboard;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableHystrixDashboard

2b9098192979f3899acb6bb8b433824850d5180c
dfw <dfwmaomao@163.com>
2020-03-02 00:18:55 +0800

    thread

---
+++ springboot_multithread/src/main/java/com/springboot/multithread/chapter1_threadknowlegde/ThreadJoin.java
@@ -0,0 +1,47 @@
+package com.springboot.multithread.chapter1_threadknowlegde;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadJoin {
+
+    public static void main(String[] args) throws Exception {
+
+
+        System.out.println("dfw==begin"+System.currentTimeMillis());
+        Thread preThread = Thread.currentThread();
+
+        for (int i = 0; i < 10; i++) {
+            Thread mThread = new Thread(new MyThread(preThread), String.valueOf(i));
+            mThread.start();
+            preThread = mThread;
+        }
+
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(Thread.currentThread().getName() + "terminate.");
+        System.out.println("dfw==end"+System.currentTimeMillis());
+    }
+
+    public static class MyThread implements Runnable {
+        private Thread mThread;
+        public MyThread(Thread mThread) {
+            this.mThread = mThread;
+        }
+
+        @Override
+        public void run() {
+            try {
+                mThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(System.currentTimeMillis());
+            System.out.println(Thread.currentThread().getName() + "terminate.");
+        }
+    }
+}
